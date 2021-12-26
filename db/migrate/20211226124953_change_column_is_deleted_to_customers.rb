498b37c257bd52942163a4404b50d12b153d5e30
yamauchimiku <yamamochi7171@gmail.com>
2021-12-26 13:02:00 +0000

    [add] is_deleted

---
+++ db/migrate/20211226124953_change_column_is_deleted_to_customers.rb
@@ -0,0 +1,2 @@
+class ChangeColumnIsDeletedToCustomers < ActiveRecord::Migration[5.2]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_25_083406) do
+ActiveRecord::Schema.define(version: 2021_12_26_125910) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2021_12_25_083406) do
     t.string "postcode"
     t.text "address"
     t.string "phone_number"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
