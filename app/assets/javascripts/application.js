d1074a4088a737d560521d2ee8206c6e8804b237
yamauchimiku <yamamochi7171@gmail.com>
2021-12-28 12:18:05 +0000

    [add] bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// bootstrapを表示させる
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
